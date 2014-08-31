a562d5f9be07c60a1d8bf49db6dfef70aaa2d633
Thomas Schätzlein <thomas@pnxs.de>
2014-08-31 22:41:31 +0200

    remove example functions

--- test/hs485Tests.js
+++ test/hs485Tests.js
@@ -3,23 +3,7 @@ var assert = require("assert");
 var hs485 = require("../");
 var util = require("util");
 
-describe("Array", function() {
-    describe("#indexOf()", function() {
-        it('should return -1 when val not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        });
-
-    });
-});
-
 describe("hs485", function() {
-    describe("myAdd", function() {
-        it('should add a+b', function() {
-            assert.equal(hs485.myAdd(1,2), 3);
-        });
-    });
-
     describe("Crc16", function() {
         it('test Crc16', function() {
             var c = new hs485.Crc16();
