0464d20e4cfb0fba3f575ae39489b7edce588c0e
Thomas Schätzlein <thomas@pnxs.de>
2014-09-21 20:50:45 +0200

    added a test for discoverModules

--- test/hs485Tests.js
+++ test/hs485Tests.js
@@ -85,4 +85,27 @@ describe("hs485", function() {
             assert.equal(r.state, hs485.request.states.Start);
         });
     });
+
+    describe("manager", function() {
+        it('discover', function() {
+            var manager = new hs485.Manager();
+
+            // expect send fe0400aed0
+
+            // push receive: 
+            // fe 00 98 00 07 80 00 00 05 4e 8d 34
+            // fe 00 98 00 07 80 00 00 05 e9 5c 6e
+            // fe 00 98 00 07 80 00 00 0c 52 de 3c
+            // fe 00 98 00 07 80 00 00 12 0a c1 78
+            // fe 00 98 00 07 80 00 00 2a 92 17 2c
+            // fe 00 98 00 07 80 00 00 2a 99 a7 3a
+            // fe 00 98 00 07 80 ff ff ff ff 4a ee (End of discovery)
+
+            manager.discoverModules(function(devList) {
+                assert.deepEqual(devList, []);
+            });
+
+
+        });
+    });
 });
