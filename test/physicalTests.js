a54251c1192f9f65e15aec8a790d3472da88abc6
Thomas Schätzlein <thomas@pnxs.de>
2014-11-02 22:09:06 +0100

    refactored testcase

--- test/physicalTests.js
+++ test/physicalTests.js
@@ -4,15 +4,20 @@ var hs485 = require("../");
 var physical = require("../lib/physical");
 
 describe("Hs485Pci driver", function() {
-    it("Open event", function(done) {
-        var sp = new hs485.mock.MockSerialPort("/dev/ttyS1", {
+    var sp;
+    var driver;
+
+    beforeEach(function() {
+        sp = new hs485.mock.MockSerialPort("/dev/ttyS1", {
             baudrate: 19200,
             parity: 'even',
             parser: hs485.parser()
         });
 
-        var driver = new physical.Hs485Pci("", sp);
+        driver = new physical.Hs485Pci("", sp);
+    });
 
+    it("Open event", function(done) {
         driver.on("open", function () {
             done();
         });
@@ -22,17 +27,9 @@ describe("Hs485Pci driver", function() {
 
 
     it("Receive frame event", function(done) {
-        var sp = new hs485.mock.MockSerialPort("/dev/ttyS1", {
-            baudrate: 19200,
-            parity: 'even',
-            parser: hs485.parser()
-        });
-
         sp.expectWrite([0xfe, 0x04, 0x00, 0xae, 0xd0], [
             [0xfe, 0x00, 0x98, 0x00, 0x07, 0x80, 0xff, 0xff, 0xff, 0xff, 0x4a, 0xee]
         ]);
-
-        var driver = new physical.Hs485Pci("", sp);
 
         driver.on("open", function () {
             driver.send([0xfe, 0x04, 0x00, 0xae, 0xd0]);
